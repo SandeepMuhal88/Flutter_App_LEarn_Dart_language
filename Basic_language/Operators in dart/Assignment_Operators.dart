90e523400b74c73d05773f2a94faed91fc5be98d
Sandeepmuhal88 <sandeepmuhal9982@gmail.com>
2025-11-05 18:02:57 +0530

    Update the code

---
+++ Basic_language/Operators in dart/Assignment_Operators.dart
@@ -0,0 +1,11 @@
+void main(){
+  print("Use Assignment Operators in Dart:");
+  int a = 10; // Simple assignment
+  int b = 20;
+  a += 5; // a = a + 5
+  b -= 3; // b = b - 3
+  
+  print("a: $a");
+  print("b: $b");
+
+}
---
+++ Basic_language/Operators in dart/Logical Operators.dart
@@ -0,0 +1,9 @@
+void main(){
+  print("Logical Operators in Dart:");
+  bool a = true;
+  bool b = false;
+  print("a && b: ${a && b}");
+  print("a || b: ${a || b}");
+  print("!a: ${!a}");
+
+}
